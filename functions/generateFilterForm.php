8fd00a41d1829f20df69359b04e4462562908ccc
Seb Lamberts <seb.lamberts@eyup.com>
2022-04-07 14:20:05 +0100

    added stored values in filter

--- functions/generateFilterForm.php
+++ functions/generateFilterForm.php
@@ -5,15 +5,36 @@
  * @return string
  */
 function generateFilterForm(array $types): string{
-    $toBeDisplayed = '<form method="get"><select name="type1"><option select="selected" value="0">Type</option>';
+
+
+    $toBeDisplayed = "<form method='get'><select name='type1'><option ";
+    if (!isset($_GET['type1']) || $_GET['type1'] == 0){
+        $toBeDisplayed.= "selected='selected' ";
+}
+    $toBeDisplayed .= "value='0'>Type</option>";
 
     foreach($types as $type){
-        $toBeDisplayed .= "<option value='{$type['id']}'>{$type['name']}</option>";
+        $toBeDisplayed .= "<option value='{$type['id']}' ";
+        if(isset($_GET['type1'])) {
+            if ($_GET['type1'] == $type['id']) {
+                $toBeDisplayed .= "selected='selected' ";
+            }
+        }
+        $toBeDisplayed .= ">{$type['name']}</option>";
     }
-    $toBeDisplayed .= "</select><select name='type2'><option select='selected' value='0'>Type</option>";
-
+    $toBeDisplayed .= "</select><select name='type2'><option ";
+    if (!isset($_GET['type2']) || $_GET['type2'] == 0){
+        $toBeDisplayed .= "selected='selected' ";
+    }
+    $toBeDisplayed .= "value='0'>Type</option>";
     foreach($types as $type){
-        $toBeDisplayed .= "<option value='{$type['id']}'>{$type['name']}</option>";
+        $toBeDisplayed .= "<option value='{$type['id']}' ";
+        if(isset($_GET['type1'])) {
+            if ($_GET['type2'] == $type['id']) {
+                $toBeDisplayed .= "selected='selected' ";
+            }
+        }
+        $toBeDisplayed .= ">{$type['name']}</option>";
     }
     $toBeDisplayed .= "</select><input type='submit' value='Filter'/></form>";
 
